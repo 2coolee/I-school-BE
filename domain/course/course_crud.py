d067dab9055a931a6b4a9386f41b16e90e61a2f5
2coolee <dltldnjs58@naver.com>
2024-07-29 15:26:49 +0900

    fix: 평균 별점 개선, 강의 시간 배열 생성

--- domain/course/course_crud.py
+++ domain/course/course_crud.py
@@ -4,7 +4,9 @@ from sqlalchemy import or_, func
 from models import Course, CourseReview
 from utils import UvicornException
 from domain.course import course_schema
-
+from sqlalchemy.sql.expression import cast
+from sqlalchemy.types import Numeric
+import statistics
 
 def read_courses(major: str, keyword: str, grade: str, db:Session):
     # 먼저 전공, 키워드, grade를 확인
@@ -26,10 +28,9 @@ def read_courses(major: str, keyword: str, grade: str, db:Session):
                     Course.name.ilike(f"%{keyword}%")
                 )
             )
-
+    
     # 쿼리 실행
     courses = query.all()
-
         # 오류 메시지 반환
     if not courses:
         raise UvicornException(status_code=400, message="강의가 존재하지 않습니다.")
@@ -37,10 +38,13 @@ def read_courses(major: str, keyword: str, grade: str, db:Session):
     # 서브쿼리: 별점 평점
     subquery = db.query(
         Course.id.label('course_id'),
-        func.avg(CourseReview.rating).label('avg_rating')
+        func.round(func.avg(CourseReview.rating), 2).label('avg_rating')
     ).join(CourseReview, Course.id == CourseReview.course_id, isouter=True).group_by(
         Course.id
     ).subquery()
+
+
+
 
         # 메인 쿼리: Course와 서브쿼리 조인
     results = db.query(
@@ -51,18 +55,27 @@ def read_courses(major: str, keyword: str, grade: str, db:Session):
         (Course.grade == grade if grade else True),
         (or_(Course.professor == keyword, Course.name == keyword) if keyword else True)
     ).order_by(
-        Course.id  # 수업을 ID로 정렬
+        Course.id # 수업을 ID로 정렬
     ).all()
 
+    # for tt in results:
+    #     print(tt.code)
+    #     print(tt.name)
+    #     print(tt.major)
+    #     print(tt.grade)
+    #     print(tt.credit)
         # 데이터 변환
     course_dict = {}
     for course, avg_rating in results:
-        if course.id not in course_dict:
-            course_dict[course.id] = {
+        print(avg_rating)
+        print(course.day)
+        if course.code not in course_dict:
+
+            course_dict[course.code] = {
                 "courseCode": course.code,
                 "courseName": course.name,
                 "professor": course.professor,
-                "rating": avg_rating,
+                "rating": [avg_rating],
                 "grade": course.grade,
                 "credit": course.credit,
                 "courseDay": [course.day],
@@ -71,10 +84,21 @@ def read_courses(major: str, keyword: str, grade: str, db:Session):
                 "courseRoom": course.course_room
             }
         else:
-            course_dict[course.id]["courseDay"].append(course.day)
-            course_dict[course.id]["courseStartTime"].append(course.start_time)
-            course_dict[course.id]["courseEndTime"].append(course.end_time)
+            course_dict[course.code]["courseDay"].append(course.day)
+            course_dict[course.code]["courseStartTime"].append(course.start_time)
+            course_dict[course.code]["courseEndTime"].append(course.end_time)
+            course_dict[course.code]["rating"].append(avg_rating)
 
+    course_dict = {
+        key: (
+            lambda course: {
+                **course,
+                "rating": statistics.mean([r for r in course["rating"] if r is not None])
+                if any(r is not None for r in course["rating"]) else None
+            } if "rating" in course and course["rating"] is not None else course
+        )(value)
+        for key, value in course_dict.items()
+    }
 
     course_list = list(course_dict.values())
 
